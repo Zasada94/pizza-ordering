9dc31ba7e842cb6f8a3116b1194eef7e4a4f1e3b
Zasada94 <pawelzasada2@wp.pl>
2023-07-20 15:16:11 +0200

    serving products from database finished

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => console.log("DB connection successfull !"))
+	.catch((err) => {
+		console.log(err);
+	});
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/products", productRoute);
+// app.use("/api/carts", cartRoute);
+// app.use("/api/orders", orderRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+	console.log("Backend server is running !");
+});
---
+++ routes/product.js
@@ -0,0 +1,40 @@
+const Product = require("../models/Product");
+
+const router = require("express").Router();
+
+//CREATE
+router.post("/", async (req, res) => {
+	const newProduct = new Product(req.body);
+	try {
+		const savedProduct = await newProduct.save();
+		res.status(200).json(savedProduct);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+//GET ALL PRODUCTS
+router.get("/", async (req, res) => {
+	const qNew = req.query.new;
+	const qCategory = req.query.category;
+	try {
+		let products;
+		if (qNew) {
+			products = await Product.find().sort({ createdAt: -1 }).limit(1);
+		} else if (qCategory) {
+			products = await Product.find({
+				categories: {
+					$in: [qCategory],
+				},
+			});
+		} else {
+			products = await Product.find();
+		}
+
+		res.status(200).json(products);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+module.exports = router;
