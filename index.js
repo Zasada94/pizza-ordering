46d96fcbc22193b9bc3f2c369169ded0aee3529d
Zasada94 <pawelzasada2@wp.pl>
2023-07-24 13:49:08 +0200

    predeploy changes

--- index.js
+++ index.js
@@ -33,4 +33,4 @@ app.listen(process.env.PORT || 5000, () => {
 	console.log("Backend server is running !");
 });
 
-export const handler = serverless(app);
+// export const handler = serverless(app);
