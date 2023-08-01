7499fa88f82acfa683d9fe2848459451d2f757e3
Zasada94 <pawelzasada2@wp.pl>
2023-08-01 10:39:44 +0200

    deploy trying

--- server.js
+++ server.js
@@ -1,39 +1,27 @@
 "use strict";
 
 const express = require("express");
+const serverless = require("serverless-http"); // Import serverless-http
+
 const app = express();
 const mongoose = require("mongoose");
 const productRoute = require("./routes/product");
-// const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const path = require("path");
-// const serverless = require("serverless-http");
 
 dotenv.config();
 
 mongoose
-	.connect(process.env.MONGO_URL)
-	.then(() => console.log("DB connection successfull !"))
-	.catch((err) => {
-		console.log(err);
-	});
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connection successful!"))
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/products", productRoute);
-// app.use("/api/carts", cartRoute);
-app.use("/api/orders", orderRoute);
-// app.use("/.netlify/functions/server", router);
-// app.use(express.static("./client/pizza-portal/dist"));
+app.use("/.netlify/functions/server/api/products", productRoute); // Adjust the base path for your routes
+app.use("/.netlify/functions/server/api/orders", orderRoute); // Adjust the base path for your routes
 
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.resolve(__dirname, "./client/pizza-portal/dist"));
-// });
-
-app.listen(process.env.PORT || 5000, () => {
-	console.log("Backend server is running !");
-});
-// module.exports = app;
-// module.exports.handler = serverless(app);
+module.exports.handler = serverless(app);
