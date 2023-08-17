b1a6153836e05e21f77b0fb18e96516674d77653
Zasada94 <92092359+Zasada94@users.noreply.github.com>
2023-08-17 15:55:45 +0200

    login backend complete

--- routes/auth.js
+++ routes/auth.js
@@ -50,7 +50,7 @@ router.post("/login", async (req, res) => {
 
 		res.status(200).json({ ...others, accessToken });
 	} catch (err) {
-		// res.status(500).json(err);
+		res.status(500).json(err);
 	}
 });
 
