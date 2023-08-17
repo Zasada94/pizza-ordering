9e7891760d392227f371de37d9d1b1d8c3d4ca10
Zasada94 <92092359+Zasada94@users.noreply.github.com>
2023-08-17 15:27:23 +0200

    cleaning, register start

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 
 const express = require("express");
 const app = express();
@@ -6,10 +6,9 @@ const mongoose = require("mongoose");
 const productRoute = require("./routes/product");
 // const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
+const authRoute = require("./routes/auth");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const path = require("path");
-// const serverless = require("serverless-http");
 
 dotenv.config();
 
@@ -25,15 +24,8 @@ app.use(express.json());
 app.use("/api/products", productRoute);
 // app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
-// app.use("/.netlify/functions/server", router);
-// app.use(express.static("./client/pizza-portal/dist"));
-
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.resolve(__dirname, "./client/pizza-portal/dist"));
-// });
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
 	console.log("Backend server is running !");
 });
-// module.exports = app;
-// module.exports.handler = serverless(app);
