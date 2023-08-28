46c2937e40ae9f9cc85957254dec10702d43d1d4
Zasada94 <pawelzasada2@wp.pl>
2023-08-28 12:07:55 +0200

    update

--- routes/order.js
+++ routes/order.js
@@ -1,19 +1,16 @@
-"use strict";
-
-const express = require("express");
 const Order = require("../models/Order");
 
-const app = express();
+const router = require("express").Router();
 
-// CREATE
-app.post("/", async (req, res) => {
-  const newOrder = new Order(req.body);
-  try {
-    const savedOrder = await newOrder.save();
-    res.status(200).json(savedOrder);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+//CREATE
+router.post("/", async (req, res) => {
+	const newOrder = new Order(req.body);
+	try {
+		const savedOrder = await newOrder.save();
+		res.status(200).json(savedOrder);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
-module.exports = app; 
+module.exports = router;
--- routes/product.js
+++ routes/product.js
@@ -1,53 +1,50 @@
-"use strict";
-
-const express = require("express");
 const Product = require("../models/Product");
 
-const app = express();
+const router = require("express").Router();
 
-// CREATE
-app.post("/", async (req, res) => {
-  const newProduct = new Product(req.body);
-  try {
-    const savedProduct = await newProduct.save();
-    res.status(200).json(savedProduct);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+//CREATE
+router.post("/", async (req, res) => {
+	const newProduct = new Product(req.body);
+	try {
+		const savedProduct = await newProduct.save();
+		res.status(200).json(savedProduct);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
-// GET ALL PRODUCTS
-app.get("/", async (req, res) => {
-  const qNew = req.query.new;
-  const qCategory = req.query.category;
-  try {
-    let products;
-    if (qNew) {
-      products = await Product.find().sort({ createdAt: -1 }).limit(1);
-    } else if (qCategory) {
-      products = await Product.find({
-        categories: {
-          $in: [qCategory],
-        },
-      });
-    } else {
-      products = await Product.find();
-    }
+//GET ALL PRODUCTS
+router.get("/", async (req, res) => {
+	const qNew = req.query.new;
+	const qCategory = req.query.category;
+	try {
+		let products;
+		if (qNew) {
+			products = await Product.find().sort({ createdAt: -1 }).limit(1);
+		} else if (qCategory) {
+			products = await Product.find({
+				categories: {
+					$in: [qCategory],
+				},
+			});
+		} else {
+			products = await Product.find();
+		}
 
-    res.status(200).json(products);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+		res.status(200).json(products);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
-// FIND PRODUCT
-app.get("/find/:id", async (req, res) => {
-  try {
-    const product = await Product.findById(req.params.id);
-    res.status(200).json(product);
-  } catch (err) {
-    res.status(500).json(err);
-  }
+//FIND PRODUCT
+router.get("/find/:id", async (req, res) => {
+	try {
+		const product = await Product.findById(req.params.id);
+		res.status(200).json(product);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
-module.exports = app;
+module.exports = router;
