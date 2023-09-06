9e4b76d3279b143f2ebf69ca3b67ff8970fe2b45
Zasada94 <pawelzasada2@wp.pl>
2023-09-06 14:02:21 +0200

    backend essentials for admin panel

--- routes/product.js
+++ routes/product.js
@@ -47,4 +47,30 @@ router.get("/find/:id", async (req, res) => {
 	}
 });
 
+// DELETE
+router.delete("/:id", async (req, res) => {
+	try {
+		await Product.findByIdAndDelete(req.params.id);
+		res.status(200).json("Product has been deleted.");
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+//UPDATE
+router.put("/:id", async (req, res) => {
+	try {
+		const updatedProduct = await Product.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set: req.body,
+			},
+			{ new: true }
+		);
+		res.status(200).json(updatedProduct);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 module.exports = router;
